7c2a31e52d1b37c9da05761b0b02aa5405c669b6
Tciganskybaron <Tihonov.n.01@gmail.com>
2024-09-06 17:30:14 +0200

    add color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import styles from './page.module.css';
 
 import { IconComponent } from '@/assets/index';
 
-const LoadingIconComponent = IconComponent.BookText;
+const LoadingIconComponent = IconComponent.Fire;
 
 export default function Home() {
   return (
     <>
-      <div className={styles.page}>HOME</div> <LoadingIconComponent />
+      <div className={styles.page}>HOME</div>
+      <LoadingIconComponent />
     </>
   );
 }
