98bee1f8a66bc60cbbaf1f86ffc5ade3e99e3127
Tciganskybaron <Tihonov.n.01@gmail.com>
2024-09-08 12:28:18 +0200

    add text constant

--- src/shared/constants/text.ts
+++ src/shared/constants/text.ts
@@ -2,6 +2,10 @@ const text = {
   LEARNING: 'Learning',
   TESTS: 'Tests',
   RAY: 'Ray Driving Theory',
+  HOME: 'Home',
+  PLAN: 'Your plan',
+  LEADERBOARD: 'Leaderboard',
+  PROFILE: 'Profile',
 };
 
 export default text;
--- src/widgets/layout/ui/FooterMain/FooterMain.tsx
+++ src/widgets/layout/ui/FooterMain/FooterMain.tsx
@@ -1,18 +1,19 @@
 import { FooterItem } from '@/entities/layout/ui';
 import styles from './FooterMain.module.css';
+import text from '@/shared/constants/text';
 
 export const FooterMain = () => {
   return (
     <div className={styles.footer}>
-      <FooterItem text={'Home'} icon={'Home'} url={'/'} active />
-      <FooterItem text={'Your plan'} icon={'Route'} url={'/'} active={false} />
+      <FooterItem text={text.HOME} icon={'Home'} url={'/'} active />
+      <FooterItem text={text.PLAN} icon={'Route'} url={'/'} active={false} />
       <FooterItem
-        text={'Leaderboard'}
+        text={text.LEADERBOARD}
         icon={'Ranking'}
         url={'/'}
         active={false}
       />
-      <FooterItem text={'Profile'} icon={'User'} url={'/'} active={false} />
+      <FooterItem text={text.PROFILE} icon={'User'} url={'/'} active={false} />
     </div>
   );
 };
